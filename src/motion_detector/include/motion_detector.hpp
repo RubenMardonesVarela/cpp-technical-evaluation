0d914af2fdc6230cce7fa62ea11f22bff35e036f
RubenMardonesVarela <ruben.mardones.varela@gmail.com>
2022-05-20 10:26:21 -0400

    Added namespace to motion_detector

--- src/motion_detector/include/motion_detector.hpp
+++ src/motion_detector/include/motion_detector.hpp
@@ -1,26 +1,32 @@
+#pragma once
 #include <vector>
 
-using namespace std;
+namespace allreadevaluation {
 
-enum class Motion{
-    MOTION,
-    NO_MOTION
-};
+enum class Motion { MOTION, NO_MOTION };
 
-template<typename A>
-class MockMotionDetector final
-{
+template <typename A> class MockMotionDetector final {
 public:
-    Motion detect(const A & previous_frame, const A & current_frame){
-        ++index;
-        return mock_motion[index%mock_motion.size()];
-    }
+  Motion detect(const A &previous_frame, const A &current_frame) {
+    ++index;
+    return mock_motion[index % mock_motion.size()];
+  }
+
 private:
-    const std::vector<Motion> mock_motion = { 
-    Motion::NO_MOTION,Motion::NO_MOTION,Motion::NO_MOTION, 
-    Motion::MOTION,Motion::MOTION, Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,
-    Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,
-    Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::MOTION,Motion::NO_MOTION, Motion::NO_MOTION, Motion::NO_MOTION};
-    
-    std::uint64_t index = 0;
+  const std::vector<Motion> mock_motion = {
+      Motion::NO_MOTION, Motion::NO_MOTION, Motion::NO_MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
+      Motion::NO_MOTION, Motion::NO_MOTION, Motion::NO_MOTION};
+
+  std::uint64_t index = 0;
 };
+
+} // namespace allreadevaluation
