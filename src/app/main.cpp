b4b2f985faddb8c8e6fbdbbfbd72d3dec778a24d
RubenMardonesVarela <ruben.mardones.varela@gmail.com>
2022-05-22 14:55:41 -0400

    Created app with two cameras

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,14 @@
 
+#include "../common/include/common.hpp"
+#include "../rtsp_motion_recorder_camera/include/rtsp_motion_camera.hpp"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
 #include "spdlog/spdlog.h"
-#include <gst/app/gstappsink.h>
 #include <gst/gst.h>
-#include <gst/video/video.h>
 #include <iostream>
 #include <unistd.h>
+
+using namespace allreadevaluation;
 
 /**
  * \brief Allread app evaluation. This app gets the rtsp stream from
@@ -17,9 +20,30 @@
  */
 int main(int argc, char *argv[]) {
 
-  auto logger = spdlog::basic_logger_mt("allreadEvaluation",
-                                        "logs/allreadEvaluation.log");
+  /* Creates a multi sink log file with output to console and file */
+  std::vector<spdlog::sink_ptr> sinks;
+  sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_mt>());
+  sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+      "logs/allreadEvaluation.log"));
+  auto loggerAllread =
+      std::make_shared<spdlog::logger>(ID_LOG, begin(sinks), end(sinks));
+  spdlog::register_logger(loggerAllread);
+  spdlog::get(ID_LOG)->info("AllreadEvaluation starts");
 
-  logger->info("AllReadEvaluation starts");
+  /* Init gstreamer */
+  gst_init(0, NULL);
+
+  RtspMotionRecorderCamera cam1{"rtsp://localhost:61250/cam01", "cam01"};
+  RtspMotionRecorderCamera cam2{"rtsp://localhost:61250/cam02", "cam02"};
+  cam1.Start();
+  cam2.Start();
+
+  std::string consoleIn = "";
+  while (consoleIn != "exit") {
+    std::cin >> consoleIn;
+  }
+
+  cam1.Stop();
+  cam2.Stop();
   return 0;
 }
