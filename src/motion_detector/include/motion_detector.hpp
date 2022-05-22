bb7bc24f6c44082a4b69a6565d8e997f8bea6335
RubenMardonesVarela <ruben.mardones.varela@gmail.com>
2022-05-22 14:54:59 -0400

    Modified motion detector to detect motion every 20 seconds

--- src/motion_detector/include/motion_detector.hpp
+++ src/motion_detector/include/motion_detector.hpp
@@ -1,30 +1,33 @@
 #pragma once
+#include <array>
 #include <vector>
 
 namespace allreadevaluation {
 
 enum class Motion { MOTION, NO_MOTION };
 
+/* Emulates motion every 10 seconds in a video with 25 fps */
 template <typename A> class MockMotionDetector final {
 public:
+  MockMotionDetector() {
+    /* No motion in first 250 frames (corresponds to
+       10 seconds in a video with 25fps) */
+    for (unsigned int i = 0; i < 250; ++i) {
+      mock_motion[i] = Motion::NO_MOTION;
+    }
+    /* Motion after 10 seconds with a duration of 10 seconds */
+    for (unsigned int i = 250; i < 500; ++i) {
+      mock_motion[i] = Motion::MOTION;
+    }
+  }
+
   Motion detect(const A &previous_frame, const A &current_frame) {
     ++index;
     return mock_motion[index % mock_motion.size()];
   }
 
 private:
-  const std::vector<Motion> mock_motion = {
-      Motion::NO_MOTION, Motion::NO_MOTION, Motion::NO_MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::MOTION,    Motion::MOTION,    Motion::MOTION,
-      Motion::NO_MOTION, Motion::NO_MOTION, Motion::NO_MOTION};
+  std::array<Motion, 500> mock_motion;
 
   std::uint64_t index = 0;
 };
